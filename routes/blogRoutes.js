55536ca7cd1799ea2a1cc65cf785d66652f55e5a
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2022-12-09 20:40:34 -0600

    typo

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,7 +3,6 @@ const blogController = require('../controllers/blogController')
 
 const router = express.Router();
 
-// router.get('/', blogController.blog_index);
 router.get('/', blogController.blog_index);
 router.post('/', blogController.blog_create_post);
 router.get('/create', blogController.blog_create_get);
